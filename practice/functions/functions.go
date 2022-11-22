2d848380185ed27d89ebffb70e4ca65517e7ff9a
nilishalu <shalini.c.e.ganu@gmail.com>
2022-11-22 23:31:28 +0530

    Practiced with variables and constants

--- practice/functions/functions.go
+++ practice/functions/functions.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"math"
 )
 
 func add(n1 int, n2 int) int {
@@ -23,10 +24,25 @@ func splitValue(sum int) (x, y int) {
 	return
 }
 
+func squareRoot(num int) (rootFloat float64, rootInt int) {
+    rootFloat = math.Sqrt(float64(num))
+	rootInt = int(rootFloat)
+	return 
+}
+
 func main() {
+	var a, b int = 5, 6
+	msg := "I can learn Go"
+    const truth = true
+	const pi = math.Pi
+
     fmt.Println("My favorite number is", rand.Intn(15))
 	// fmt.Println(add(5, -2))
 	fmt.Println(multiply(4, 8))
 	fmt.Println(swapNames("Shalu", "Shiva"))
 	fmt.Println(splitValue(17))
+	fmt.Printf("%d %d \n", a, b)
+	fmt.Println(msg)
+	fmt.Println(squareRoot(6))
+	fmt.Printf("%v %v", pi, truth)
 }
