e70fdee83946080f762b843843a866ce3a35d9dd
nilishalu <shalini.c.e.ganu@gmail.com>
2022-11-23 22:16:48 +0530

    practiced with arrays

---
+++ practice/arrays.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sliceAsReference() {
+	var names = []string {
+		"John",
+		"Anshu",
+		"Rahul",
+		"Pragya",
+		"Prajakta",
+	}
+
+	a := names[0:2]
+	b := names[1:]
+	fmt.Println(a, b, names)
+
+	b[0] = "NoName"
+
+	fmt.Println(a,b,names)
+}
+
+func main() {
+	var a = [4]int{4, 5,23, 32}
+	fmt.Println(a)
+	var s[]int = a[0:2]
+	last := []int(a[3:])
+	fmt.Println(s, last)
+	sliceAsReference()
+}
